70907a6a20c20c79c17c9cce23c4690075711436
Superamaja <38116622+Superamaja@users.noreply.github.com>
2023-03-11 00:25:03 -0600

    added choosing activate and deactivate

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import json
 import os
 
-configFile = 'config.json'
+configFile = 'config2.json'
 
 try:
     with open(configFile) as f:
--- startup-config.py
+++ startup-config.py
@@ -4,13 +4,31 @@ from win32com.client import Dispatch
 # Get shell:startup path using os
 shellPath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
 
-path = shellPath + "/thing.lnk" # Path to be saved (shortcut)
-target = os.getcwd() + "/main (no-console).pyw"  # The shortcut target file or folder
-workDir = os.getcwd()  # The parent folder of your file
+path = shellPath + "/Discord-PC-Remote.lnk"
+target = os.getcwd() + "/main (no-console).pyw"
+workDir = os.getcwd() 
 
 if os.path.exists('main (no-console).pyw'):
-    shell = Dispatch('WScript.Shell')
-    shortcut = shell.CreateShortCut(path)
-    shortcut.Targetpath = target
-    shortcut.WorkingDirectory = workDir
-    shortcut.save()
+    choice = None
+    while (not choice in ['1', '2']):
+        choice = input('(1) Activate startup with windows\n(2) Deactivate startup with windows\nEnter your choice: ')
+        if choice == '1':
+            if os.path.exists(path):
+                os.remove(path) # Incase it's a broken shortcut
+            shell = Dispatch('WScript.Shell')
+            shortcut = shell.CreateShortCut(path)
+            shortcut.Targetpath = target
+            shortcut.WorkingDirectory = workDir
+            shortcut.save()
+            print('Startup with windows activated!')
+            input('Press enter to exit...')
+        elif choice == "2":
+            if os.path.exists(path):
+                os.remove(path)
+                print('Startup with windows deactivated!')
+                input('Press enter to exit...')
+            else:
+                print('Startup with windows is already deactivated!')
+                input('Press enter to exit...')
+else:
+    print('main (no-console).pyw not found! Please reinstall the full program from GitHub.')
